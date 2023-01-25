ed1b7bc6c14b1e67ef5a1ecb753632fdab32232c
HJ S. Miranda <hmiranda@moylanmnl.net>
2023-01-25 11:38:50 +0800

    fixing eslints error

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,8 @@
 'use client';
 
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 
-import { textContainer, textVariant2 } from '../utils/motion'
-
+import { textContainer, textVariant2 } from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
@@ -22,10 +21,10 @@ export const TypingText = ({ title, textStyles }) => (
 export const TitleText = ({ title, textStyles }) => (
   <motion.h2
     variants={textVariant2}
-    initial='hidden'
-    whileInView='show'
+    initial="hidden"
+    whileInView="show"
     className={`mt-[8px] font-bold md:text-[64px] text=[40px] text-white ${textStyles}`}
   >
-      {title}
-    </motion.h2>
+    {title}
+  </motion.h2>
 );
