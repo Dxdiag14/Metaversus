330d2426e3b547f33f39be7c075357a5adf88e76
HJ S. Miranda <hmiranda@moylanmnl.net>
2023-01-26 15:12:43 +0800

    fixing more nextjs errors on spaces, commas and indents

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,22 +1,22 @@
 'use client';
 
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-import styles from "../styles";
-import { fadeIn, staggerContainer, zoomIn } from "../utils/motion";
+import styles from '../styles';
+import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
 
 const Feedback = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <motion.div
       variants={staggerContainer}
-      initial='hidden'
-      whileInView='show'
-      viewport = {{once: false, amount: 0.25}}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col lg:flex-row gap-6`}
     >
       <motion.div
         variants={fadeIn('right', 'tween', 0.2, 1)}
-        className='flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-dash border-[#6A6A6A] relative'
+        className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-dash border-[#6A6A6A] relative"
       >
         <div className="feedback-gradient" />
         <div>
@@ -24,16 +24,14 @@ const Feedback = () => (
           <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white">Founder of Metaversus</p>
         </div>
 
-        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45px] leading-[39px] text-white">
-        “With the development of today's technology, metaverse is very useful for today's work, or can be called web 3.0. by using metaverse you can use it as anything”
-        </p>
+        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45px] leading-[39px] text-white">“With the development of today's technology, metaverse is very useful for today's work, or can be called web 3.0. by using metaverse you can use it as anything”</p>
       </motion.div>
 
       <motion.div
         variants={fadeIn('left', 'tween', 0.2, 1)}
-        className='relative flex-1 flex justify-center items-center'
+        className="relative flex-1 flex justify-center items-center"
       >
-        <img 
+        <img
           src="/planet-09.png"
           alt="planet09"
           className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
@@ -41,9 +39,9 @@ const Feedback = () => (
 
         <motion.div
           variants={zoomIn(0.4, 1)}
-          className='lg:block hidden absolute -left-[10%] top-[3%]'
+          className="lg:block hidden absolute -left-[10%] top-[3%]"
         >
-          <img 
+          <img
             src="/stamp.png"
             alt="stamp"
             className="w-[155px] h-[155px] object-contain"
